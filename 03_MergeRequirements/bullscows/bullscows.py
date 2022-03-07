42aa0b64ceaf509fe5be23dc36aef85f86213a8c
sanyavertolet <lxnfrolov@gmail.com>
2022-03-07 21:11:09 +0300

    Small cosmetic changes due to module name conflict

--- 03_MergeRequirements/bullscows/bullscows.py
+++
@@ -1,8 +0,0 @@
-import textdistance
-def bullscows(guess: str, secret: str) -> (int, int):
-    bulls = textdistance.levenshtein.similarity(guess, secret)
-    cows = textdistance.bag.similarity(guess, secret)
-    return bulls, cows
-
-if __name__ == '__main__':
-    print(bullscows('ропот', 'полип'))
