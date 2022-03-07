135d991e86b00e96439eedff3839343cb1483c0d
sanyavertolet <lxnfrolov@gmail.com>
2022-03-07 22:35:53 +0300

    Prepared NameGen class skeleton

---
+++ 04_MergetoolCommandline/NameGen.py
@@ -0,0 +1,19 @@
+import cmd
+import pynames
+import shlex
+
+class NameGen(cmd.Cmd):
+    language = pynames.LANGUAGE.NATIVE
+    
+
+    def do_language(self, args):
+        pass
+
+    def do_generate(self, args):
+        pass
+
+
+    def do_info(self, args):
+        pass
+
+
