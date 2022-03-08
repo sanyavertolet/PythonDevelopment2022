3832377aeeac0cf721a27a01155dbc67ce68a163
sanyavertolet <lxnfrolov@gmail.com>
2022-03-08 12:44:17 +0300

    Implemented info command

--- 04_MergetoolCommandline/NameGen.py
+++ 04_MergetoolCommandline/NameGen.py
@@ -4,6 +4,7 @@ import pynames
 import shlex
 
 class NameGen(cmd.Cmd):
+    abstract_generators = ['FromTablesGenerator', 'FromListGenerator', 'FromCSVTablesGenerator']
     language = pynames.LANGUAGE.NATIVE
     prompt = '(NameGen) '   
 
@@ -21,7 +22,6 @@ class NameGen(cmd.Cmd):
 
 
     def do_generate(self, args):
-        AbstractGenerators = ['FromTablesGenerator', 'FromListGenerator', 'FromCSVTablesGenerator']
         args = shlex.split(args)
         module = importlib.import_module(f'pynames.generators.{args[0].lower()}')
         if args[0] == 'elven' and len(args) > 1:
@@ -29,7 +29,7 @@ class NameGen(cmd.Cmd):
         elif args[0] == 'iron_kingdoms' and len(args) > 1:
             generator = args[1] + 'FullnameGenerator'
         else:
-            generator = [gen for gen in dir(module) if gen.endswith('Generator') and gen not in AbstractGenerators][0]
+            generator = [gen for gen in dir(module) if gen.endswith('Generator') and gen not in self.abstract_generators][0]
         generator = getattr(module, generator)
         gender = pynames.GENDER.FEMALE if args[-1] == 'female' else pynames.GENDER.MALE
         try:
@@ -40,6 +40,22 @@ class NameGen(cmd.Cmd):
 
 
     def do_info(self, args):
-        pass
+        args = shlex.split(args)
+        module = importlib.import_module(f'pynames.generators.{args[0].lower()}')
+        if args[0] == 'elven' and len(args) > 1:
+            generator = args[1] + 'NamesGenerator'
+        elif args[0] == 'iron_kingdoms' and len(args) > 1:
+            generator = args[1] + 'FullnameGenerator'
+        else:
+            generator = [gen for gen in dir(module) if gen.endswith('Generator') and gen not in self.abstract_generators][0]
+        generator = getattr(module, generator)
+        if args[-1] == 'language':
+            print(*sorted(generator().languages))
+        elif args[-1] == 'male':
+            print(generator().get_names_number(pynames.GENDER.MALE))
+        elif args[-1] == 'female':
+            print(generator().get_names_number(pynames.GENDER.FEMALE))
+        else:
+            print(generator().get_names_number())
 
 NameGen().cmdloop()
